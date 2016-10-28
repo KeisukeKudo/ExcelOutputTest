eca9a9b26c068c7686c446be0adfc6db484a11dd
keisuke kudo <melchior.balthasar.casper326@gmail.com>
2016-10-28 23:35:30 +0900

    first commit

---
+++ ExcelOutputTest/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using OfficeOpenXml;
+using System.Diagnostics;
+using System.Data;
+using System.IO;
+
+namespace ExcelOutputTest {
+    class Program {
+
+        private const string ExcelFileName = "test.xlsx";
+        private const string SheetName = "test";
+
+        static void Main(string[] args) {
+
+            if (File.Exists(ExcelFileName)) {
+                File.Delete(ExcelFileName);
+            }
+
+            DataTable outputData = CreateTestData();
+
+            var s = new Stopwatch();
+            s.Start();
+
+            var outputFile = new FileInfo(ExcelFileName);
+            using (var book = new ExcelPackage(outputFile))
+            using (var sheet = book.Workbook.Worksheets.Add(SheetName)) {
+                CreateExcel(outputData, book, sheet);
+            }
+
+            s.Stop();
+            Console.WriteLine($"終了({ s.ElapsedMilliseconds.ToString("#,0") }ms)");
+
+        }
+
+        /// <summary>
+        /// 200 * 30,000のデータ作成
+        /// </summary>
+        /// <returns></returns>
+        static DataTable CreateTestData() {
+            var dt = new DataTable();
+
+            for (var i = 0; i < 200; i++) {
+                dt.Columns.Add($"カラム{ i }");
+            }
+
+            for (var i = 0; i < 30000; i++) {
+                var dr = dt.NewRow();
+                for (var j = 0; j < dt.Columns.Count; j++) {
+                    dr[j] = $"テストデータ{ i }_{ j }";
+                }
+
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+
+        /// <summary>
+        /// (200 * 30,000) + 200 = 6,000,200セルにデータをつっこむ
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="book"></param>
+        /// <param name="sheet"></param>
+        static void CreateExcel(DataTable dt, ExcelPackage book, ExcelWorksheet sheet) {
+            //カラム名設定
+            for (var x = 1; x <= dt.Columns.Count; x++) {
+                sheet.Cells[1, x].Value = dt.Columns[x - 1].ColumnName;
+            }
+
+            //データ設定
+            for (var y = 2; y <= dt.Rows.Count + 1; y++) {
+
+                var row = dt.Rows[y - 2];
+
+                for (var x = 1; x <= dt.Columns.Count; x++) {
+
+                    sheet.Cells[y, x].Value = row[x - 1];
+
+                }
+            }
+            //セル幅調整 割りとアテにならない
+            sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
+            book.Save();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# ExcelOutputTest
+とある理由でExcel出力のパフォーマンスをテスト  
+使ったライブラリは[EPPlus](http://epplus.codeplex.com/ "EPPlus")  
+
+# 結果
+作成時間: 209,339ms(4分弱)  
+![result1](https://github.com/KeisukeKudo/ImageStorage/blob/master/ExcelOutputTestResult1.png)  
+
+作成Excelファイル  
+![result2](https://github.com/KeisukeKudo/ImageStorage/blob/master/ExcelOutputTestResult2.png)  
